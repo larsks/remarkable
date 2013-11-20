901470b7f9b26e9bd42a7b3bf05ea40b8517887f
Lars Kellogg-Stedman <lars@redhat.com>
2013-11-20 11:17:28 -0500

    need to get bind address from environment

--- app.py
+++ app.py
@@ -10,5 +10,7 @@ except IOError:
     pass
 
 from app import app
-app.run(server="gevent", port=os.environ.get("OPENSHIFT_PYTHON_PORT", 8080))
+app.run(server='gevent',
+        host=os.environ.get('OPENSHIFT_PYTHON_IP', '127.0.0.1),
+        port=os.environ.get('OPENSHIFT_PYTHON_PORT', 8080))
 
