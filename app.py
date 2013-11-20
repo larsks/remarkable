0b09f5d5406829dd5ba0794454e4d927591feb15
Lars Kellogg-Stedman <lars@redhat.com>
2013-11-20 11:13:27 -0500

    use template code from wsgi/application

--- app.py
+++ app.py
@@ -2,6 +2,13 @@
 
 import os
 
+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
+
 from app import app
 app.run(server="gevent", port=os.environ.get("OPENSHIFT_PYTHON_PORT", 8080))
 
