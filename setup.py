c403b21e3111a1c076eb4c2a9150d182b11ecb41
Lars Kellogg-Stedman <lars@redhat.com>
2013-11-21 09:59:41 -0500

    typo

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(name='Remarkable',
       description='Shared slideshows',
       author='Lars Kellogg-Stedman',
       author_email='lars@oddbit.com',
-      url='http://blog.oddbit.com/",
+      url='http://blog.oddbit.com/',
       install_requires=['gevent', 'bottle', 'pyzmq', 'jinja2'],
      )
