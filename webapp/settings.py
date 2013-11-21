83eb47741ddb978a1ca1abdd7c2bab7ba0259ead
Lars Kellogg-Stedman <lars@redhat.com>
2013-11-21 10:47:02 -0500

    rename openshift_use_alt_port to using_openshift

--- webapp/settings.py
+++ webapp/settings.py
@@ -2,7 +2,10 @@
 
 import os
 
-openshift_use_alt_port = 'OPENSHIFT_APP_NAME' in os.environ
+# We need to know when we are running under OpenShift
+# because of socket handling issues in the default
+# frontend.
+using_openshift = 'OPENSHIFT_APP_NAME' in os.environ
 
 static_dir = os.path.join(
         os.environ.get('OPENSHIFT_REPO_DIR', '.'),
